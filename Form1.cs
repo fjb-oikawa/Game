2b2f2713632b2517378e2a5f0dc3ea6efa1946c3
oikawa <oikawa@chiba-fjb.ac.jp>
2017-06-02 12:38:43 +0900

    ミサイル追加

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace Game
 {
     public partial class Form1 : Form
     {
-        int speed = 10;
+        int speed = 5;
         int moveX;
         int moveY;
         public Form1()
@@ -67,6 +67,11 @@ namespace Game
             if (e.KeyCode == Keys.Right)
             {
                 moveX = speed;
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                pictureBox2.Left = pictureBox1.Left;
+                pictureBox2.Top = pictureBox1.Top;
             }
         }
 
